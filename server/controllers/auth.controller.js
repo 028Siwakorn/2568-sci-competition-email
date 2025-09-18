2b259a83f1c84b37108119e676bd3ebeedb2782e
Siwakorn Kleebmek <664259028@webmail.npru.ac.th>
2025-09-18 12:01:16 +0700

    latest/verify

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import authConfig from "../config/auth.config.js";
 import jwt from "jsonwebtoken";
 const User = db.User;
 import crypto from "crypto";
+import path from "path";
 import { sendVerificationEmail } from "../utils/email.js";
 
 //Register
@@ -92,8 +93,51 @@ const signUp = async (req, res) => {
   }
 };
 
+const verifyEmail = async (req, res) => {
+  const { token } = req.params;
+  if (!token) {
+    return res.status(400).send({ message: "Token undefined!" });
+  }
+
+  try {
+    const verificationToken = await db.verificationToken.findOne({
+      where: { token },
+    });
+    if (!verificationToken) {
+      return res.status(404).send({
+        message: "Invalid Verification Token!",
+      });
+    }
+    //Check if Token is Expired
+    if (new Date() > verificationToken.expiredAt) {
+      await verificationToken.destroy();
+      return res
+        .status(400)
+        .send({ message: "Verification Token has expired!" });
+    }
+    const user = await User.findByPk(verificationToken.userId);
+    if (!user) {
+      return res.status(404)({ message: "User not found" });
+    }
+    //Change verify status
+    await user.update({ isVerified: true });
+    await verificationToken.destroy();
+    //return web view
+    const htmlPath = path.join(
+      process.cwd(),
+      "views",
+      "verification-success.html"
+    );
+    res.sendFile(htmlPath);
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message || "Some error occurred while verifying the user",
+    });
+  }
+};
 const authController = {
   signUp,
+  verifyEmail,
 };
 
 export default authController;
--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -4,6 +4,9 @@ import authController from "../controllers/auth.controller.js";
 //POST http://localhost:500/api/v1/auth/signup
 router.post("/signup", authController.signUp);
 
+//GET http://localhost:500/api/v1/auth/verify/:token
+router.get("/verify/:token", authController.verifyEmail);
+
 //POST http://localhost:500/api/v1/auth/signup
 //router.post("/signin", authController.signIn);
 
